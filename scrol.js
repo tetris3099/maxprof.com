a104fc03b312038f44ce6173d27bb5229b34bbe5
tetris3099 <117729047+tetris3099@users.noreply.github.com>
2025-02-07 22:12:15 +0200

    scrol.js

--- scrol.js
+++ scrol.js
@@ -1,68 +1,78 @@
 const cardBox = document.querySelector('.price-repair');
 
-const a = [
+const cardRadio = {
+ radioOne: document.getElementById('card-radio-one'),
+ radioTwo: document.getElementById('card-radio-two'),
+ radioThree: document.getElementById('card-radio-three'),
+ radioFour: document.getElementById('card-radio-four'),
+};
 
-  document.getElementById('card-one').addEventListener('click', function name() {
-    cardBox.scrollTo(0, 0)}),
+const clickScrol = (element, x) => {
+  element.addEventListener('click', function name() {
+    cardBox.scrollTo(x, 0)})
+};
 
-  document.getElementById('card-two').addEventListener('click', function name() {
-    cardBox.scrollTo(320, 0)}),
-
-    document.getElementById('card-three').addEventListener('click', function name() {
-      cardBox.scrollTo(620, 0)}),
-
-      document.getElementById('card-four').addEventListener('click', function name() {
-        cardBox.scrollTo(860, 0)})
-];
+clickScrol(cardRadio.radioOne, 0);
+clickScrol(cardRadio.radioTwo, 320);
+clickScrol(cardRadio.radioThree, 620);
+clickScrol(cardRadio.radioFour, 1000);
 
 // CARDS
-function iconShow() {
+const iconShow = () => {
   const cardBoxScrolled = Number(cardBox.scrollLeft.toFixed(0));
   const cardBoxWidth = cardBox.scrollWidth - cardBox.clientWidth;
   const res = Number(((cardBoxScrolled / cardBoxWidth) * 100).toFixed(0));
-  console.log(res);
 
-  if(res > 85 && res === 100) {
-    document.getElementById('card-four').classList.add('check');
-    document.getElementById('card-three').classList.remove('check');
-    document.getElementById('card-two').classList.remove('check');
-    document.getElementById('card-one').classList.remove('check');
+  const {radioOne, radioTwo, radioThree, radioFour} = cardRadio
+  if(res > 85 && res < 100) {
+    radioFour.classList.add('check');
+    radioThree.classList.remove('check');
+    radioTwo.classList.remove('check');
+    radioOne.classList.remove('check');
   } else  if(res > 50 && res < 85) {
-  document.getElementById('card-three').classList.add('check');
-  document.getElementById('card-one').classList.remove('check');
-  document.getElementById('card-two').classList.remove('check');
-  document.getElementById('card-four').classList.remove('check');
-} else if(res > 12 && res < 49) {
-  document.getElementById('card-two').classList.add('check');
-  document.getElementById('card-four').classList.remove('check');
-  document.getElementById('card-three').classList.remove('check');
-  document.getElementById('card-one').classList.remove('check');
-} else if(res === 0 && res < 12) {
-  document.getElementById('card-one').classList.add('check');
-  document.getElementById('card-two').classList.remove('check');
-  document.getElementById('card-three').classList.remove('check');
-  document.getElementById('card-four').classList.remove('check');
-}};
+    radioThree.classList.add('check');
+    radioOne.classList.remove('check');
+    radioTwo.classList.remove('check');
+    radioFour.classList.remove('check');
+  } else if(res > 12 && res < 49) {
+    radioTwo.classList.add('check');
+    radioFour.classList.remove('check');
+    radioThree.classList.remove('check');
+    radioOne.classList.remove('check');
+  } else if(res > 0 && res < 12) {
+    radioOne.classList.add('check');
+    radioTwo.classList.remove('check');
+    radioThree.classList.remove('check');
+    radioFour.classList.remove('check');
+  }
+};
 
 cardBox.addEventListener('scroll', iconShow)
 
-// TO TOP
-const b = document.querySelector('.to-top').addEventListener('click', function name() {
-  window.scrollTo(0, 0)
-});
+const butCards = {
+  buttOne: document.getElementById('butt-card-one'),
+  buttTwo: document.getElementById('butt-card-two'),
+  buttThree: document.getElementById('butt-card-three'),
+  buttFour: document.getElementById('butt-card-four'),
+};
 
-function nameToTop() {
-  const a = document.body.scrollTop || document.documentElement.scrollTop;
-  const b = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const c = (a / b * 100).toFixed(0);
-  const res = Number(c)
-
-  if(res > 30) {
-    document.querySelector('.to-top').classList.remove('no')
-  } else if(res < 30 || res === 0) {
-    document.querySelector('.to-top').classList.add('no')
+const clickButt = (click) => {
+  click.addEventListener('click', whatButton)
+  const {buttOne, buttTwo, buttThree, buttFour} = butCards;
+  function whatButton () {
+    if (click === buttOne) {
+      buttOne.textContent = '90 999 грн';
+    } else if (click === buttTwo) {
+      buttTwo.textContent = '150 000 грн';
+    } else if (click === buttThree) {
+      buttThree.textContent = '185 000 грн';
+    } else if (click === buttFour) {
+      buttFour.textContent = '237 000 грн';
+    }
   }
-}
+};
 
-window.addEventListener('scroll', nameToTop);
-
+clickButt(butCards.buttOne);
+clickButt(butCards.buttTwo);
+clickButt(butCards.buttThree);
+clickButt(butCards.buttFour);
